8c604d714bd2dab5094b417d54366990bcfc9672
MartinNyberg <55439200+MartinNyberg@users.noreply.github.com>
2019-10-22 13:03:07 +0200

    Fighting sim

---
+++ November_projekt/November_projekt/Fighter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace November_projekt
+{
+    class Fighter
+    {
+        private int hp = 1000;
+
+        public string name = "";
+
+        public string GiveName()
+        {
+            name = Console.ReadLine();
+            Console.WriteLine("Du gav namnet " + name);
+
+            return name;
+        }
+
+
+        //random generatorn för dmg
+        public int Attack()
+        {
+            Random generator = new Random();
+            int damage = generator.Next(250, 600);
+
+            return damage;
+        }
+
+        //Hurtmetoden drar hp och gör så hp inte går under 0
+        public void Hurt(int amount)
+        {
+            hp = hp - amount;
+            Console.WriteLine(name + " tog " + amount + " skada.");
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+
+        }
+
+        //skriver current hp
+        public int GetHp()
+        {
+            Console.WriteLine(name + " har nu " + hp + "hp kvar.");
+
+            return hp;
+        }
+
+        //kollar om man lever eller inte
+        public bool isAlive()
+        {
+            bool alive = false;
+
+            if (hp >= 1)
+            {
+                alive = true;
+            }
+
+            return alive;
+        }
+
+
+    }
+}
---
+++ November_projekt/November_projekt/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace November_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fighter A = new Fighter();
+            Fighter B = new Fighter();
+
+            //ger namn åt spelaren
+            Console.Write("Välj namne på spelare 1:");
+            string namnA = A.GiveName();
+            Console.Write("Välj namne på spelare 2:");
+            string namnB = B.GiveName();
+            Console.WriteLine("Nu börjar kampen!");
+
+            Console.ReadKey();
+
+            bool playerAisAlive = A.isAlive();
+
+            bool playerBisAlive = B.isAlive();
+
+            while (playerAisAlive == true && playerBisAlive == true)
+            //Spelare attackerar om båda lever
+            {
+                playerBisAlive = B.isAlive();
+                int attack = A.Attack();
+                B.Hurt(attack);
+                B.GetHp();
+                playerBisAlive = B.isAlive();
+                Console.ReadKey();
+                attack = B.Attack();
+
+                playerAisAlive = A.isAlive();
+                A.Hurt(attack);
+                A.GetHp();
+                playerAisAlive = A.isAlive();
+                Console.ReadKey();
+            }
+
+            //Skriver vilekn spelare som vann
+            if (playerAisAlive == true && playerBisAlive == false)
+            {
+                Console.WriteLine(namnA + " vann!");
+            }
+            else if (playerBisAlive == true && playerAisAlive == false)
+            {
+                Console.WriteLine(namnB + " vann!");
+            }
+            else
+            {
+                Console.WriteLine("oavgjort");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
